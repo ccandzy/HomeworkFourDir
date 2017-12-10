26d891bc4a34f889f75f9eb05fc06ac156c2a1c3
ccandzy <32675034+ccandzy@users.noreply.github.com>
2017-12-10 22:58:29 +0800

    1210修改
    ThreadPool的封装 等待某条任务完成 或者等待全部任务完成
    1000 耗时任务， 完成部分

---
+++ HomeworkFour/HomeworkFour/Common/ThreadPoolEx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeworkFour.Common
+{
+    public class ThreadPoolEx
+    {
+        private ManualResetEvent eventX = new ManualResetEvent(false);
+        private ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+        private ManualResetEvent manualResetEventForWhenAll = new ManualResetEvent(false);
+        private static object _lockObject = new object(); 
+        private List<Action> actionList = new List<Action>();
+
+        /// <summary>
+        /// 带回调函数的ThreadPool
+        /// </summary>
+        /// <param name="action">执行方法</param>
+        /// <param name="callBack">回调方法</param>
+        public void ThreadPoolWithCallBack(Action<object> action, Action callBack)
+        {
+            ThreadPool.QueueUserWorkItem((obj) =>
+            {
+                action.Invoke(obj);
+                callBack.Invoke();
+            });
+        }
+
+        /// <summary>
+        /// 带返回参数的ThreadPool
+        /// </summary>
+        /// <param name="func">传入一个带有返回值的委托</param>
+        /// <returns>返回一个泛型委托，必须执行才能得到结果</returns>
+        public Func<T> ThreadPoolWithReturn<T>(Func<object, T> func)
+        {
+            T t = default(T);
+            ThreadPool.QueueUserWorkItem((obj) =>
+            {
+                t = func.Invoke(obj);
+                eventX.Set();
+            });
+            return new Func<T>(() =>
+            {
+                eventX.WaitOne();
+                eventX.Reset();
+                return t;
+            });
+        }
+
+        /// <summary>
+        ///  带返回参数,回调函数的ThreadPool
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="callBack"></param>
+        /// <returns></returns>
+        public Func<T> ThreadPoolWithReturnCallBack<T>(Func<object, T> func, Action callBack)
+        {
+            T t = default(T);
+            ThreadPool.QueueUserWorkItem((obj) =>
+            {
+                t = func.Invoke(obj);
+                callBack.Invoke();
+                eventX.Set();
+
+            });
+            return new Func<T>(() =>
+            {
+                return t;
+            });
+        }
+
+        /// <summary>
+        /// 创建一个线程
+        /// </summary>
+        /// <param name="action"></param>
+        public void CreateThread(Action action)
+        {
+            ThreadPool.QueueUserWorkItem((obj)=> 
+            {
+                action.Invoke();
+                manualResetEvent.Set();
+                DeteleAction(action);
+            });
+        }
+
+        /// <summary>
+        /// 其中某一条任务完成执行
+        /// </summary>
+        /// <param name="action"></param>
+        public void ContinueWhenAny(Action action)
+        {
+            ThreadPool.QueueUserWorkItem((obj)=>
+            {
+                manualResetEvent.WaitOne();
+                manualResetEvent.Reset();
+                action.Invoke();
+            });
+        }
+        
+        /// <summary>
+        /// 所有任务完成执行
+        /// </summary>
+        /// <param name="action"></param>
+        public void ContinueWhenAll(Action action)
+        {
+            ThreadPool.QueueUserWorkItem((obj) =>
+            {
+                manualResetEventForWhenAll.WaitOne();
+                manualResetEventForWhenAll.Reset();
+                action.Invoke();
+            });
+        }
+
+        public void AddWaitThread(Action action)
+        {
+            actionList.Add(action);
+        }
+
+        private void DeteleAction(Action action)
+        {
+            lock(_lockObject)
+            {
+                actionList.Remove(action);
+                if(!actionList.Any())
+                {
+                    manualResetEventForWhenAll.Set();
+                }
+            }
+        }
+    }
+}
--- HomeworkFour/HomeworkFour/Program.cs
+++ HomeworkFour/HomeworkFour/Program.cs
@@ -16,11 +16,15 @@ namespace HomeworkFour
     class Program
     {
 
-        private static bool _isStart = false;
         private static object _lockOne = new object();
+
+        static List<Action> listAction = new List<Action>();
+        static List<Task> task = new List<Task>();
+
+        private static object _taskLock = new object();
         static void Main(string[] args)
         {
-            #region 第一版
+            #region 故事实现第一版
             //QiaoFeng qiaoFeng = new QiaoFeng();
             //XuZhu xuZhu = new XuZhu();
             //DuanYu duanYu = new DuanYu();
@@ -83,105 +87,229 @@ namespace HomeworkFour
 
             #endregion
 
-            #region 升级版1
-            
-            ConfigHelper.CreateData();
-            Dictionary<string,List<string>> dictionary = ConfigHelper.GetDictionary();
-            CancellationTokenSource cts = new CancellationTokenSource();
+            #region 故事实现升级版
 
-            Stopwatch stopwatchUpgradeOne = new Stopwatch();
-            stopwatchUpgradeOne.Start();
+            //ConfigHelper.CreateData();
+            //Dictionary<string,List<string>> dictionary = ConfigHelper.GetDictionary();
+            //CancellationTokenSource cts = new CancellationTokenSource();
 
-            Task listenTask = new Task(() =>
-            {
-                while (!cts.IsCancellationRequested)
-                {
-                    Thread.Sleep(800);
-                    int randomInt = new Random().Next(0, 2020);
-                    //Console.WriteLine(randomInt);
-                    if (randomInt == 2017)
-                    {
-                        cts.Cancel();
-                        Console.WriteLine("世界末日！！！");
-                    }
-                }
-            });
-            listenTask.Start();
+            //Stopwatch stopwatchUpgradeOne = new Stopwatch();
+            //stopwatchUpgradeOne.Start();
+
+            //Task listenTask = new Task(() =>
+            //{
+            //    while (!cts.IsCancellationRequested)
+            //    {
+            //        Thread.Sleep(800);
+            //        int randomInt = new Random().Next(0, 2020);
+            //        //Console.WriteLine(randomInt);
+            //        if (randomInt == 2017)
+            //        {
+            //            cts.Cancel();
+            //            Console.WriteLine("世界末日！！！");
+            //        }
+            //    }
+            //});
+            //listenTask.Start();
 
 
-            Task write = new Task(() =>
-            {
-                while(!cts.IsCancellationRequested)
-                {
-                    Thread.Sleep(500);
-                    LogHelper.CustomConsoleWirte();
-                }
-            });
-            write.Start();
+            //Task write = new Task(() =>
+            //{
+            //    while(!cts.IsCancellationRequested)
+            //    {
+            //        Thread.Sleep(500);
+            //        LogHelper.CustomConsoleWirte();
+            //    }
+            //});
+            //write.Start();
 
-            Action<string> storyNodeUpgradeOne = (node) =>
-            {
-                try
-                {
-                    var randomInt = new RandomHelper().GetNumber(1, 5000);
-                    Thread.Sleep(randomInt);
-                    if (cts.IsCancellationRequested) return;
-                    if (!_isStart)
-                    {
-                        lock (_lockOne)
-                        {
-                            LogHelper.WriteTxtLog(node + $"等{randomInt}秒，进了LOCK");
-                            if (!_isStart)
-                            {
-                                _isStart = true;
-                                LogHelper.WriteTxtLog("******天龙八部拉开序幕******");
-                            }
-                        }
-                    }
-                    else
-                    {
-                        LogHelper.WriteTxtLog(node + $"等{randomInt}秒，直接出去");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    cts.Cancel();
-                    throw;
-                }
-                
-            };
-            var taskFactoryUpgradeOne = new TaskFactory();
-            
-            var taskListUpgradeOne = dictionary.Select(dic => taskFactoryUpgradeOne.StartNew(() =>
-                {
-                    dic.Value.ForEach(storyNodeUpgradeOne);
-                    return dic.Key;
-                })).ToList();
-            var endTask = new Task(() => LogHelper.WriteTxtLog("中原群雄大战辽兵,忠义两难一死谢天"), cts.Token);
+            //Action<string> storyNodeUpgradeOne = (node) =>
+            //{
+            //    try
+            //    {
+            //        var randomInt = new RandomHelper().GetNumber(1, 5000);
+            //        Thread.Sleep(randomInt);
+            //        if (cts.IsCancellationRequested) return;
+            //        if (!_isStart)
+            //        {
+            //            lock (_lockOne)
+            //            {
+            //                LogHelper.WriteTxtLog(node + $"等{randomInt}秒，进了LOCK");
+            //                if (!_isStart)
+            //                {
+            //                    _isStart = true;
+            //                    LogHelper.WriteTxtLog("******天龙八部拉开序幕******");
+            //                }
+            //            }
+            //        }
+            //        else
+            //        {
+            //            LogHelper.WriteTxtLog(node + $"等{randomInt}秒，直接出去");
+            //        }
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e);
+            //        cts.Cancel();
+            //        throw;
+            //    }
 
-            try
-            {
-                Task.Factory.ContinueWhenAny(taskListUpgradeOne.ToArray(), t =>LogHelper.WriteTxtLog(t.Result.ToString() + "已经做好准备啦"),cts.Token);
-                Task.Factory.ContinueWhenAll(taskListUpgradeOne.ToArray(), t => endTask.Start(), cts.Token);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            try
-            {
-                Task.WaitAll(endTask);
-                stopwatchUpgradeOne.Stop();
-                LogHelper.WriteTxtLog("******全剧时长:" + stopwatchUpgradeOne.Elapsed + "******");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.InnerException.Message);
-            }
+            //};
+            //var taskFactoryUpgradeOne = new TaskFactory();
+
+            //var taskListUpgradeOne = dictionary.Select(dic => taskFactoryUpgradeOne.StartNew(() =>
+            //    {
+            //        dic.Value.ForEach(storyNodeUpgradeOne);
+            //        return dic.Key;
+            //    })).ToList();
+            //var endTask = new Task(() => LogHelper.WriteTxtLog("中原群雄大战辽兵,忠义两难一死谢天"), cts.Token);
+
+            //try
+            //{
+            //    Task.Factory.ContinueWhenAny(taskListUpgradeOne.ToArray(), t =>LogHelper.WriteTxtLog(t.Result.ToString() + "已经做好准备啦"),cts.Token);
+            //    Task.Factory.ContinueWhenAll(taskListUpgradeOne.ToArray(), t => endTask.Start(), cts.Token);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+            //try
+            //{
+            //    Task.WaitAll(endTask);
+            //    stopwatchUpgradeOne.Stop();
+            //    LogHelper.WriteTxtLog("******全剧时长:" + stopwatchUpgradeOne.Elapsed + "******");
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.InnerException.Message);
+            //}
             #endregion
+
+            #region ThreadPool 带参数，返回值 ContinueWhenAll ContinueWhenAny实现
+
+            Console.WriteLine("Main ThreadID:" + Thread.CurrentThread.ManagedThreadId+"begin");
+
+            ThreadPoolEx threadPoolEx = new ThreadPoolEx();
             
+            Console.WriteLine("**********带回调的ThreadTool**********");
+            threadPoolEx.ThreadPoolWithCallBack(Dosomething, CallBackMethod);
+            
+            Console.WriteLine("**********带带返回参数的ThreadPool**********");
+            var iResult = threadPoolEx.ThreadPoolWithReturn(DosomethingWithReturn);
+            var resultValue = iResult.Invoke();
+            Console.WriteLine("result value:" + resultValue);
+            
+            Console.WriteLine("**********带返回参数,回调函数的ThreadPool**********");
+            var iResultWithReturnCallBack = threadPoolEx.ThreadPoolWithReturnCallBack(DosomethingWithReturn, CallBackMethod);
+            var resultValueWithReturnCallBack = iResultWithReturnCallBack.Invoke();
+            Console.WriteLine("result value:" + resultValueWithReturnCallBack);
+
+            Console.WriteLine("**********ContinueWhenAny    ContinueWhenAll的实现**********");
+            threadPoolEx.AddWaitThread(ThreadThree); //加入要等待的线程 ContinueWhenAll 需要加入等待列表
+            threadPoolEx.AddWaitThread(ThreadTwo);//加入要等待的线程 ContinueWhenAll 需要加入等待列表
+
+            threadPoolEx.CreateThread(ThreadOne);//通过CreateThread方法创建的，其中一个完成就会触发ContinueWhenAny
+            threadPoolEx.CreateThread(ThreadTwo);//通过CreateThread方法创建的，其中一个完成就会触发ContinueWhenAny
+            threadPoolEx.CreateThread(ThreadThree);//通过CreateThread方法创建的，其中一个完成就会触发ContinueWhenAny
+            threadPoolEx.ContinueWhenAny(() => 
+            {
+                Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create whenAny begin");
+                Console.WriteLine("whenAny excute");
+                Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create whenAny end");
+            });
+
+            threadPoolEx.ContinueWhenAll(() => 
+            {
+                Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create whenAll begin");
+                Console.WriteLine("whenAll excute");
+                Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create whenAll end");
+            });
+            Console.WriteLine("Main ThreadID:" + Thread.CurrentThread.ManagedThreadId+"end");
+            #endregion
+
+            #region 1000任务实现
+            
+           
+            for(int i = 0; i<100; i++)
+            {
+                Action acttion = new Action(() => Console.WriteLine(i.ToString()));
+                listAction.Add(acttion);
+            }
+
+          
+            for(int j=0;j<10; j++)
+            {
+                Task taskItem = new Task(listAction[j]).ContinueWith(t =>
+                {
+                    lock(_taskLock)
+                    {
+                        listAction.Remove(listAction[j]);
+                    }
+                });
+                taskItem.Start();
+            }
+
+            //while (listAction.Count == 0)
+            //{
+
+            //}
+
+            #endregion
             Console.ReadKey();
+        }
+        
+        static void ContinueTask(Task task)
+        {
+            if(listAction.Count > 0)
+            {
+                //int index = listAction[]
+                task = new Task(listAction[0]).ContinueWith(t =>
+                {
+                    lock (_taskLock)
+                    {
+                        //listAction.Remove(listAction[j]);
+                    }
+                });
+                task.Start();
+            }
+        }
+
+        static void Dosomething(object obj)
+        {
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(3000);
+            Console.WriteLine("Dosomething");
+        }
+        static void CallBackMethod()
+        {
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("This is CallBackMethod");
+        }
+        static int DosomethingWithReturn(object obj)
+        {
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(3000);
+            int value = new RandomHelper().GetNumber(1, 10);
+            Console.WriteLine("create value:"+value);
+            return value;
+        }
+        private static void ThreadOne()
+        {
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create 1 begin");
+            Thread.Sleep(5000);
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create 1 end");
+        }
+        private static void ThreadTwo()
+        {
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create 2 begin");
+            Thread.Sleep(3000);
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create 2 end");
+        }
+        private static void ThreadThree()
+        {
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create 3 begin");
+            Thread.Sleep(1000);
+            Console.WriteLine("ThreadID:" + Thread.CurrentThread.ManagedThreadId + "begin create 3 end");
         }
     }
 }
